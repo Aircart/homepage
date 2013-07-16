3869b23ee5ce2bba61867ec1b0893e148448d5b8
Tarik Ansari <tarik@tajn.net>
2013-07-16 04:58:21 -0400

    updated Amazon account

--- config.rb
+++ config.rb
@@ -88,8 +88,8 @@ end
 activate :s3_sync do |s3_sync|
   s3_sync.bucket                = 'aircartapp.com' # The name of the S3 bucket you are targetting. This is globally unique.
   s3_sync.region                = 'us-east-1'     # The AWS region for your bucket.
-  s3_sync.aws_access_key_id     = 'AKIAJ364I6JONIYFIEFA'
-  s3_sync.aws_secret_access_key = '9XKAq3bD7vLsOfCtlya97OJd/zgbkMqZL1PHbrqM'
+  s3_sync.aws_access_key_id     = 'AKIAJFU3X3MOUGRQKHRQ'
+  s3_sync.aws_secret_access_key = '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'
   s3_sync.delete                = false # We delete stray files by default.
   s3_sync.after_build           = false # We chain after the build step by default. This may not be your desired behavior...
   s3_sync.prefer_gzip           = true
